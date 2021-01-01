c785b51c28cd5fb1059cd92f5728f2cd3cb69698
Ayan Majumdar <heresyourayan@gmail.com>
2021-01-01 11:14:32 +0530

    Initial commit

---
+++ src/qapi.js
@@ -0,0 +1,25 @@
+export default {
+	"pages": {
+		"root": {
+			"id": "root",
+			"pageUrl": "root",
+			"name": "root",
+			"children": [
+				"5fedf465acd62200175d05a9",
+				"5fedf465acd62200175d05ab"
+			]
+		},
+		"5fedf465acd62200175d05a9": {
+			"id": "5fedf465acd62200175d05a9",
+			"name": "404",
+			"pageUrl": "404"
+		},
+		"5fedf465acd62200175d05ab": {
+			"id": "5fedf465acd62200175d05ab",
+			"name": "index",
+			"pageUrl": "index"
+		}
+	},
+	"mode": "production",
+	"projectType": "create-react-app"
+}
